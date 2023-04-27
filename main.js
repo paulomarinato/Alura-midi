378ef806f04f8e9a53c40fa445a76cc67473a20d
paulomarinato <marinatopaulo@gmail.com>
2023-04-27 00:17:41 -0300

    Conclusão do projeto.

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {       
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
@@ -13,6 +20,18 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onclick = function () {
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 
 }
